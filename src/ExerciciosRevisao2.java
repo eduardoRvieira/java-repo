802b787dba6237de20d970429aebe67a92cce9d2
Eduardo Reichel Vieira <eduardoreichel05@gmail.com>
2026-07-20 20:42:30 -0300

    Exercício hipotenusa
    Adicionando exercício 1 de cálculo de hipotenusa em Java

--- src/ExerciciosRevisao2.java
+++ src/ExerciciosRevisao2.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class ExerciciosRevisao2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		 
+		 ex1(sc);
+		 
+		 sc.close();
+	}
+	
+	public static void ex1(Scanner sc) {
+		System.out.println("=== Exercício 1 ===");
+		
+		System.out.println("Insira o primeiro cateto: ");
+		double a = sc.nextDouble();
+		System.out.println("Insira o segundo cateto: ");
+		double b = sc.nextDouble();
+		
+		double hipotenusa = Math.sqrt((a * a) + (b * b));
+		
+		System.out.println("A hipotenusa é: " + hipotenusa);
 	}
 
 }
