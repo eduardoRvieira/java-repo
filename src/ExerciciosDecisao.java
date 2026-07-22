7444f343d22f0b94d81829014efb1d2d47719e92
Eduardo Reichel Vieira <eduardoreichel05@gmail.com>
2026-07-22 19:54:46 -0300

    Update ExerciciosDecisao.java

--- src/ExerciciosDecisao.java
+++ src/ExerciciosDecisao.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class ExerciciosDecisao {
 
 	public static void main(String[] args) {
-
-
+		Scanner sc = new Scanner(System.in);
+		
+		ex1(sc);
+		}
+	
+	public static void ex1(Scanner sc) {
+		System.out.println("=== Exercício 1 ===");
+		
+		System.out.println("Insira um número inteiro para verificar se é par ou ímpar: ");
+		int n = sc.nextInt();
+		
+		if (n % 2 == 0) {
+			System.out.println("O número é par.");
+		}
+		else {
+			System.out.println("O número é ímpar.");
+		}
+			
 	}
 
 }
