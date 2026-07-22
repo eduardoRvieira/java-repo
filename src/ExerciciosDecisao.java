34e44fb37f372b44354ad8122953a76e9a025abe
Eduardo Reichel Vieira <eduardoreichel05@gmail.com>
2026-07-22 20:31:54 -0300

    Update ExerciciosDecisao.java

--- src/ExerciciosDecisao.java
+++ src/ExerciciosDecisao.java
@@ -6,7 +6,8 @@ public class ExerciciosDecisao {
 		Scanner sc = new Scanner(System.in);
 		
 		ex1(sc);
-		}
+		ex2(sc);
+	}
 	
 	public static void ex1(Scanner sc) {
 		System.out.println("=== Exercício 1 ===");
@@ -20,7 +21,29 @@ public class ExerciciosDecisao {
 		else {
 			System.out.println("O número é ímpar.");
 		}
-			
+	}
+	
+	public static void ex2(Scanner sc) {
+		System.out.println("=== Exercício 2 ===");
+		
+		System.out.println("Insira a idade (em anos) do nadador para saber sua categoria: ");
+		int idade = sc.nextInt();
+		
+		if (idade <= 7) {
+			System.out.println("Categoria Infantil A");
+		}
+		else if (idade <= 10) {
+			System.out.println("Categoria Infantil B");
+		}
+		else if (idade <= 13) {
+			System.out.println("Categoria Juvenil A");
+		}
+		else if (idade <= 17) {
+			System.out.println("Categoria Juvenil B");
+		}
+		else {
+			System.out.println("Categoria Adulto");
+		}
 	}
 
 }
