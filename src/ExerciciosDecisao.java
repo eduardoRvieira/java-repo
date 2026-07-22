2b71cc8bf459279fcf582413c24dda0aa7f0cd38
Eduardo Reichel Vieira <eduardoreichel05@gmail.com>
2026-07-22 20:46:23 -0300

    Update ExerciciosDecisao.java

--- src/ExerciciosDecisao.java
+++ src/ExerciciosDecisao.java
@@ -7,6 +7,7 @@ public class ExerciciosDecisao {
 		
 		ex1(sc);
 		ex2(sc);
+		ex3(sc);
 	}
 	
 	public static void ex1(Scanner sc) {
@@ -24,7 +25,7 @@ public class ExerciciosDecisao {
 	}
 	
 	public static void ex2(Scanner sc) {
-		System.out.println("=== Exercício 2 ===");
+		System.out.println("\n=== Exercício 2 ===");
 		
 		System.out.println("Insira a idade (em anos) do nadador para saber sua categoria: ");
 		int idade = sc.nextInt();
@@ -45,5 +46,36 @@ public class ExerciciosDecisao {
 			System.out.println("Categoria Adulto");
 		}
 	}
-
+	
+	public static void ex3(Scanner sc) {
+		System.out.println("\n=== Exercício 3 ===");
+		
+		System.out.println("=== CÁLCULO DE PREÇO DE CAIXA ===");
+		System.out.println("\n1 - Laranja");
+		System.out.println("2 - Limão");
+		System.out.println("3 - Morango");
+		System.out.println("Escolha: ");
+		int opcao = sc.nextInt();
+		System.out.println("Insira o valor unitário do fruto: ");
+		double valor = sc.nextDouble();
+		double preco_caixa = 0;
+		
+		switch(opcao) {
+			case 1:
+				preco_caixa = valor*60;
+				System.out.println("O valor de uma caixa de laranja é: R$" + preco_caixa);
+				break;
+			case 2:
+				preco_caixa = valor*80;
+				System.out.println("O valor de uma caixa de limão é: R$" + preco_caixa);
+				break;
+			case 3:
+				preco_caixa = valor*20;
+				System.out.println("O valor de uma caixa de morango é: R$" + preco_caixa);
+				break;
+			default:
+				System.out.println("Insira uma opção válida.");
+				break;
+		}
+	}
 }
