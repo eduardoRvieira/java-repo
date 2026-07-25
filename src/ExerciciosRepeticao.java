02307dd71f6e236609c71320383a2b531d53f521
Eduardo Reichel Vieira <eduardoreichel05@gmail.com>
2026-07-24 21:00:58 -0300

    Update ExerciciosRepeticao.java

--- src/ExerciciosRepeticao.java
+++ src/ExerciciosRepeticao.java
@@ -5,6 +5,22 @@ public class ExerciciosRepeticao {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		ex1(sc);
+
+	}
+	
+	public static void ex1(Scanner sc) {
+		double chico = 150;
+		double juca = 110;
+		int ano = 0;
+		
+		while (juca <= chico) {
+			chico += 2;
+			juca += 3;
+			ano++;
+		}
+		
+		System.out.println("Juca fica mais alto que Chico em " + ano + " anos.");
 	}
 
 }
