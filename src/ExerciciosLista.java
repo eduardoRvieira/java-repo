be6b7d351f8b112b48e65aa4a3c13a76f0414663
Eduardo Reichel Vieira <146209138+eduardoRvieira@users.noreply.github.com>
2026-07-27 10:55:04 -0300

    Update ExerciciosLista.java

--- src/ExerciciosLista.java
+++ src/ExerciciosLista.java
@@ -11,8 +11,9 @@ public class ExerciciosLista {
 			System.out.println("\n=================================");
 			System.out.println("== MENU DE EXERCÍCIOS - ARRAYS ==");
 			System.out.println("=================================");
-			System.out.println("1 - Média de Ímpares");
-			System.out.println("2 - Contador de idades +35 anos");
+			System.out.println("1 - Média de Ímpares em Vetor");
+			System.out.println("2 - Contador de idades +35 anos em Vetor");
+			System.out.println("3 - Soma, Contagem e Média Condicional em Vetor");
 			System.out.println("0 - Sair do programa");
 	        System.out.println("---------------------------------");
 	        System.out.println("Escolha uma opção: ");
@@ -24,6 +25,9 @@ public class ExerciciosLista {
 				break;
 			case 2:
 				ex2(sc);
+				break;
+			case 3:
+				ex3(sc);
 				break;
 			case 0:
 				System.out.println("Programa encerrado.");
@@ -42,7 +46,7 @@ public class ExerciciosLista {
 	public static void ex1(Scanner sc) {
 		System.out.println("\n=== Exercício 1 ===");
 		
-		int numeros[] = {1, 2, 3, 41};
+		int numeros[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		int qtdeNumeros = numeros.length;
 		
 		int somaNumeros = 0;
@@ -77,6 +81,34 @@ public class ExerciciosLista {
 			
 		}
 		System.out.println("\nA quantidade de pessoas com idade superior a 35 é: " + qtdePessoasSup35);	
+	}
+	
+	public static void ex3(Scanner sc) {
+		System.out.println("\n=== Exercício 3 ===");
+		
+		int A[] = {15, 16, 15, 14, 12, 13, 17, 18, 19, 11};
+		int qtdeA = A.length;
+		int somaAInf15 = 0;
+		int qtdeAIgual15 = 0;
+		int somaASup15 = 0;
+		int qtdeASup15 = 0;
+		
+		for (int i = 0; i < qtdeA; i++) {
+			if (A[i] < 15) {
+				somaAInf15 += A[i];
+			}else if(A[i] == 15) {
+				qtdeAIgual15++;
+			} else {
+				somaASup15 += A[i];
+				qtdeASup15++;
+			}
+		}
+		double mediaASup15 = (double) somaASup15/qtdeASup15;
+		
+		System.out.printf("\nA soma de elementos armazenados neste vetor que são inferiores a 15: %d%n", somaAInf15);
+		System.out.printf("A quantidade de elementos armazenados no vetor que são iguais a 15: %d%n", qtdeAIgual15);
+		System.out.printf("A média dos elementos armazenados no vetor que são superiores a 15: %.2f%n", mediaASup15);
+		
 	}
 
 }
