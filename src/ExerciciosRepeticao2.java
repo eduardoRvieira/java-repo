07d2839a76ce459d24623b9bce2ab2cbaff48a67
Eduardo Reichel Vieira <eduardoreichel05@gmail.com>
2026-07-27 20:37:50 -0300

    Create ExerciciosRepeticao2.java

---
+++ src/ExerciciosRepeticao2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ExerciciosRepeticao2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		ex1(sc);
+	}
+	
+	public static void ex1(Scanner sc) {
+		
+	}
+
+}
